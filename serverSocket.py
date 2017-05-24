e6a25730d3867664ee3024460e351037ee63d712
Aashish <aashishdhungana@gmail.com>
2017-05-24 17:55:21 -0500

    added 17:55

--- serverSocket.py
+++ serverSocket.py
@@ -2,11 +2,15 @@
 
 #Server Socket
 import socket
+#import clientSocket.py
 
-def initialize(port):
+
+#server is listening
+def initializeServer(port):
 	s = socket.socket();
-	hostname = s.gethostname();
-	s.bind(hostname,port);
+	#socket.setblocking(0);
+	hostname = socket.gethostname();
+	s.bind((hostname,port));
 	s.listen(5);
 	while True :
 		c, addr = s.accept();
@@ -14,3 +18,23 @@ def initialize(port):
 		c.send("ACK");
 		c.close();
 	return hostname;
+
+
+def main():
+
+	print "Waiting For Client";
+	hostname = initializeServer(8080);
+	packetReceived = s.recieve(1024);
+	print packetReceived;
+	if(packetReceived["intm"] == None):
+		#mk_packet("", 0, "", "");
+		print "Empty packet Recieved";
+	else:
+		print "delivered";
+	print hostname;
+
+if __name__ == "__main__":
+	main();
+
+
+
